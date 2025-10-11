532e5c81ab5bc984489a819ac20cdb3f5e811cc3
Bhuwanyu <agrabhuwanyu@gmail.com>
2025-10-11 21:38:12 +0530

    5th Commit Update

--- Day 5/Sprite 2D Animation Samurai (Assignment)/sketch.js
+++ Day 5/Sprite 2D Animation Samurai (Assignment)/sketch.js
@@ -4,6 +4,7 @@ let count = 0;
 let row = 0;
 let x = 0, y = 0;
 let xdir = 0, ydir = 0;
+let a=0;
 
 function preload() {
   //load the full sprite image
@@ -11,7 +12,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(innerWidth, innerHeight);
+  createCanvas(innerWidth, 116);
   //break the sprite image into individual images and store it into a 2D array (Note - 2D array is basically where each element fo the arrat is another array)
   // w -> width of each individual image
   // h -> height of each individual image
@@ -34,6 +35,14 @@ function setup() {
 
 function draw() {
   background(255,0,0);
+  a=a+0.07;
+  noStroke();
+  fill(255);
+  ellipse(innerWidth-140-a,90,160);
+  noFill();
+  strokeWeight(4);
+  stroke(1)
+  line(innerWidth*0,116,innerWidth,116); 
   //draw the appropriate image from the array based on the row selected (check keyPressed) and the x and y position
   image(sprites[row][count],x,y);
   if(frameCount%5==0 && keyPressed) {
@@ -47,33 +56,52 @@ function draw() {
 }
 
 function keyPressed() {
+ 
   //read the appropriate row based on the key direction
-  if(keyIsDown(16) && (keyIsDown(37) || keyIsDown(65))) {//left arrow or 'a' key37 65
-    row = 11; // select the right row
-    xdir = -15; //make sure the sprite doesnt move along x axis
-    ydir = 0; //make sprite move up
+  if(keyIsDown(71)) {//g key to attack
+    row = 3; 
+    xdir = 0; 
+    ydir = 0;
+
+} else if(keyIsDown(16) && (keyIsDown(37) || keyIsDown(65))) {//left arrow or 'a' key + shift key to sprint
+    row = 11; 
+    xdir = -15; 
+    ydir = 0; 
   }
-  else if(keyIsDown(37)||keyIsDown(65)) {//shift key
+  else if(keyIsDown(37)||keyIsDown(65)) {//left arrow or 'a' key to walk left
     row = 10;
     ydir = 0; //make sure the sprite doesnt move along y axis
     xdir = -7;
-     //make sprite run left
 } else if(keyCode ==   32 ) {//space key to jump
     row = 7;
-    xdir = 0; //make sure the sprite doesnt move along x axis
-    ydir = 0; //make sprite move down
+    xdir = 0; 
+    ydir = 0; 
   } else if(keyIsDown(16) && (keyIsDown(68) || keyIsDown(39))) {//shift key + right arrow or 'd' key to sprint 
     row = 2;
     ydir = 0; //make sure the sprite doesnt move along y axis
     xdir = 15;
      //make sprite move left
-  } else if(keyIsDown(39) || keyIsDown (68)) {//right arrow or 'd' key
+} else if(keyIsDown(39) || keyIsDown (68)) {//right arrow or 'd' key
     row = 8;
     ydir = 0;
     row = 1;
     ydir = 0; //make sure the sprite doesnt move along y axis
     xdir = 7; //make sprite move left
   } 
+}
+function mousePressed() {
+  if (mouseButton === LEFT) {
+row = 3; 
+    xdir = 0; 
+    ydir = 0;
+}
+}
+function mouseReleased() {
+  if (mouseButton === LEFT) {
+row = 0; 
+    xdir = 0; 
+    ydir = 0;
+}
 }
 function keyReleased() {
   xdir = 0;
