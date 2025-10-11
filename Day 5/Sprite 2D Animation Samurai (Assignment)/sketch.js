5e1a2586e348f60f54092606e1471aff0db8cb32
Bhuwanyu <agrabhuwanyu@gmail.com>
2025-10-11 22:05:56 +0530

    5th Commit 2nd Update

--- Day 5/Sprite 2D Animation Samurai (Assignment)/sketch.js
+++ Day 5/Sprite 2D Animation Samurai (Assignment)/sketch.js
@@ -34,11 +34,11 @@ function setup() {
 }
 
 function draw() {
-  background(255,0,0);
+  background(255-a*2,0,0,255-a+5);
   a=a+0.07;
   noStroke();
-  fill(255);
-  ellipse(innerWidth-140-a,90,160);
+  fill(255-a);
+  ellipse(innerWidth-140-a,90+a+0.3,160);
   noFill();
   strokeWeight(4);
   stroke(1)
